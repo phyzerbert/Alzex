cc084e109d3499a2fa7136461e01be3f7a96be68
phyzerbert <xian1017@outlook.com>
2019-06-21 02:55:14 +0800

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use Nexmo;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -25,12 +26,13 @@ class HomeController extends Controller
      */
     public function index(Request $request) {        
         config(['site.page' => 'home']);
+        $search_users = User::pluck('id')->toArray();
         $from = $to = $period = '';        
         if ($request->get('period') != ""){   
             $period = $request->get('period');
             $from = substr($period, 0, 10);
             $to = substr($period, 14, 10);
         }
-        return view('home', compact('period', 'from', 'to'));
+        return view('home', compact('period', 'search_users', 'from', 'to'));
     } 
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,7 +48,7 @@
                     <div class="form-search">
                         <form action="" class="form-inline" method="post">
                             @csrf
-                            <input type="text" name="period" id="overview-period" class="form-control form-control-sm period" value="{{$period}}" placeholder="Timestamp" style="min-width:200px;">
+                            <input type="text" name="period" id="overview-period" class="form-control form-control-sm period" value="{{$period}}" placeholder="Timestamp" autocomplete="off" style="min-width:200px;">
                             <button type="submit" class="btn btn-primary btn-sm ml-2"><i class="icon-search4"></i> Search</button>
                         </form>
                     </div>
@@ -92,6 +92,42 @@
                 <div class="card-body">
                     <div class="chart-container">
                         <div class="chart has-fixed-height" id="overview_chart"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header header-elements-inline">
+                    <h5 class="card-title">Overview</h5>
+                    <div class="form-search">
+                        <form action="" class="form-inline" method="post">
+                            @csrf
+                            <input type="text" name="period" id="overview-period" class="form-control form-control-sm period" value="{{$period}}" placeholder="Timestamp" autocomplete="off" style="min-width:200px;">
+                            <button type="submit" class="btn btn-primary btn-sm ml-2"><i class="icon-search4"></i> Search</button>
+                        </form>
+                    </div>
+                    <div class="header-elements">
+                        <div class="list-icons">
+                            <a class="list-icons-item" data-action="collapse"></a>
+                            <a class="list-icons-item" data-action="reload"></a>
+                            <a class="list-icons-item" data-action="remove"></a>
+                        </div>
+                    </div>
+                </div>
+                @php
+                    $user_key_array = $user_expense_array = $user_incoming_array = array();
+                    for ($i=0; $i < count($search_users); $i++) { 
+                        $item = \App\User::find($search_users[$i]);
+                        array_push($user_key_array, $item->name);
+                        $user_expense = $item->transactions()->where('type', 1)->whereBetween('timestamp', [$from, $to])->sum('amount');
+                        $user_incoming = $item->transactions()->where('type', 2)->whereBetween('timestamp', [$from, $to])->sum('amount');
+                        array_push($user_expense_array, $user_expense);
+                        array_push($user_incoming_array, $user_incoming);
+                    }
+                @endphp
+
+                <div class="card-body">
+                    <div class="chart-container">
+                        <div class="chart has-fixed-height" id="user_chart"></div>
                     </div>
                 </div>
             </div>
@@ -268,14 +304,188 @@
                 };
             };
 
-
-            //
-            // Return objects assigned to module
-            //
-
             return {
                 init: function() {
                     dashboard_chart();
+                }
+            }
+        }();
+
+        var EchartsColumnsWaterfalls = function() {
+
+            var _columnsWaterfallsExamples = function() {
+                if (typeof echarts == 'undefined') {
+                    console.warn('Warning - echarts.min.js is not loaded.');
+                    return;
+                }
+
+                // Define elements
+                var columns_basic_element = document.getElementById('user_chart');
+
+                // Basic columns chart
+                if (columns_basic_element) {
+
+                    // Initialize chart
+                    var columns_basic = echarts.init(columns_basic_element);
+
+                    // Options
+                    columns_basic.setOption({
+
+                        // Define colors
+                        color: ['#2ec7c9','#b6a2de','#5ab1ef','#ffb980','#d87a80'],
+
+                        // Global text styles
+                        textStyle: {
+                            fontFamily: 'Roboto, Arial, Verdana, sans-serif',
+                            fontSize: 13
+                        },
+
+                        // Chart animation duration
+                        animationDuration: 750,
+
+                        // Setup grid
+                        grid: {
+                            left: 0,
+                            right: 40,
+                            top: 35,
+                            bottom: 0,
+                            containLabel: true
+                        },
+
+                        // Add legend
+                        legend: {
+                            data: ['Expense', 'Incoming'],
+                            itemHeight: 8,
+                            itemGap: 20,
+                            textStyle: {
+                                padding: [0, 5]
+                            }
+                        },
+
+                        // Add tooltip
+                        tooltip: {
+                            trigger: 'axis',
+                            backgroundColor: 'rgba(0,0,0,0.75)',
+                            padding: [10, 15],
+                            textStyle: {
+                                fontSize: 13,
+                                fontFamily: 'Roboto, sans-serif'
+                            }
+                        },
+
+                        // Horizontal axis
+                        xAxis: [{
+                            type: 'category',
+                            data: {!! json_encode($user_key_array) !!},
+                            axisLabel: {
+                                color: '#333'
+                            },
+                            axisLine: {
+                                lineStyle: {
+                                    color: '#999'
+                                }
+                            },
+                            splitLine: {
+                                show: true,
+                                lineStyle: {
+                                    color: '#eee',
+                                    type: 'dashed'
+                                }
+                            }
+                        }],
+
+                        // Vertical axis
+                        yAxis: [{
+                            type: 'value',
+                            axisLabel: {
+                                color: '#333'
+                            },
+                            axisLine: {
+                                lineStyle: {
+                                    color: '#999'
+                                }
+                            },
+                            splitLine: {
+                                lineStyle: {
+                                    color: ['#eee']
+                                }
+                            },
+                            splitArea: {
+                                show: true,
+                                areaStyle: {
+                                    color: ['rgba(250,250,250,0.1)', 'rgba(0,0,0,0.01)']
+                                }
+                            }
+                        }],
+
+                        // Add series
+                        series: [
+                            {
+                                name: 'Expense',
+                                type: 'bar',
+                                data: {!! json_encode($user_expense_array) !!},
+                                itemStyle: {
+                                    normal: {
+                                        label: {
+                                            show: true,
+                                            position: 'top',
+                                            textStyle: {
+                                                fontWeight: 500
+                                            }
+                                        }
+                                    }
+                                },
+                                markLine: {
+                                    data: [{type: 'average', name: 'Average'}]
+                                }
+                            },
+                            {
+                                name: 'Incoming',
+                                type: 'bar',
+                                data: {!! json_encode($user_incoming_array) !!},
+                                itemStyle: {
+                                    normal: {
+                                        label: {
+                                            show: true,
+                                            position: 'top',
+                                            textStyle: {
+                                                fontWeight: 500
+                                            }
+                                        }
+                                    }
+                                },
+                                markLine: {
+                                    data: [{type: 'average', name: 'Average'}]
+                                }
+                            }
+                        ]
+                    });
+                }
+
+                var triggerChartResize = function() {
+                    columns_basic_element && columns_basic.resize();
+                };
+
+                // On sidebar width change
+                $(document).on('click', '.sidebar-control', function() {
+                    setTimeout(function () {
+                        triggerChartResize();
+                    }, 0);
+                });
+
+                // On window resize
+                var resizeCharts;
+                window.onresize = function () {
+                    clearTimeout(resizeCharts);
+                    resizeCharts = setTimeout(function () {
+                        triggerChartResize();
+                    }, 200);
+                };
+            };
+
+            return {
+                init: function() {
+                    _columnsWaterfallsExamples();
                 }
             }
         }();
@@ -286,6 +496,7 @@
 
         document.addEventListener('DOMContentLoaded', function() {
             EchartsAreas.init();
+            EchartsColumnsWaterfalls.init();
         });
 
     </script>
