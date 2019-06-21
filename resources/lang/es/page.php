94feb95e5c9b52da7c274b3b017eaef863255f45
phyzerbert <xian1017@outlook.com>
2019-06-21 22:18:10 +0800

    edit transaction

--- resources/lang/es/page.php
+++ resources/lang/es/page.php
@@ -15,6 +15,11 @@ return [
     'please_enter_your_verification_code.' => 'Por favor ingrese su código de verificación',
     'verification_code' => 'Código de verificación',
     'verify' => 'Verificar',
+    
+    // ********  Header  ********
+    'online' => 'Online',
+    'my_profile' => 'My Profile',
+    'logout' => 'Logout',
 
     // ********  Sidebar  ********
     'dashboard' => 'Tablero',
@@ -22,5 +27,81 @@ return [
     'user' => 'Usuaria',
     'category' => 'Categoría',
     'account' => 'Cuenta',
+
+    
+    // ******** Common ********
+    'home' => 'Home',
+    'account_balance' => 'Account Balance',
+    'expense' => 'Expense',
+    'incoming' => 'Incoming',
+    'transfer' => 'Transfer',
+    'search' => 'Search',
+    'reset' => 'Reset',
+    'action' => 'Action',
+    'add_new' => 'Add New',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'save' => 'Save',
+    'close' => 'Close',
+    'back' => 'Back',
+    'name' => 'Name',
+    'type' => 'Type',
+    'total' => 'Total',
+    'items' => 'Items',
+    'all' => 'All',
+    'comment' => 'Comment',
+    'are_you_sure' => 'Are you sure?',
+    'accepted_formats_image' => 'Accepted formats: gif, png, jpg. Max file size 2Mb.',
+    'success' => 'Success',
+    'error' => 'Error',
+    'created_successfully' => 'Created Successfully',
+    'updated_successfully' => 'Updated Successfully',
+    'deleted_successfully' => 'Deleted Successfully',
+    'insufficient' => 'Insufficient',
+    'insufficient_balance' => 'Insufficient Balance',
+
+    // ******** Dashboard ********
+    'overview' => 'Overview',
+    'user_overview' => 'User Overview',
+    'category_overview' => 'Category Overview',
+
+    // ******** Transaction ********
+    'select_type' => 'Select a type',
+    'select_account' => 'Select an account',
+    'select_category' => 'Select a category',
+    'timestamp' => 'Timestamp',
+    'amount' => 'Amount',
+    'withdraw_from' => 'Withdraw From',
+    'date' => 'Date',
+    'target_account' => 'Target Account',
+    'attachment' => 'Attachment',
+    'description' => 'Description',
+    'expenses' => 'Expenses',
+    'incomes' => 'Incomes',
+    'profit' => 'Profit',
+
+    // ******** Category ********
+
+    
+    // ******** Account ********
+    'group' => 'Group',
+    'add_group' => 'Add Group',
+    'add_account'=> 'Add Account',
+    'add_new_group'=> 'Add New Group',
+    'add_new_account'=> 'Add New Account',
+
+    // ******** User ********
+    'username' => 'Username',
+    'role' => 'Role',
+    'phone_number' => 'Phone Number', 
+    'password_confirm' => 'Password Confirm', 
+    'add_new_user' => 'Add New User',
+    'edit_user' => 'Edit User',
+    'profile' => 'Profile',
+    'profile_picture' => 'Profile Picture',
+    'new_password' => 'New Password',
+    'confirm_password' => 'Confirm Password',
+    'save_changes' => 'Save Changes',
+
 
 ];
